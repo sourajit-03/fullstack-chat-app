5fd072a65996e5f5d22fdfa59165ab4183af720d
Sourajit Paul <sourajitpaul7@gmail.com>
2026-03-25 19:31:03 +0530

    Changes Made

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,11 +23,8 @@ app.use(
   }),
 );
 
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-
 
 app.use(cookieParser());
 
@@ -38,7 +34,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
